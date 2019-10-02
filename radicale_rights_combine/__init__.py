b7008cc7e01750f51de032c51077062af65f2643
Finn Krein <finnkrein@googlemail.com>
2019-10-02 13:07:48 +0200

    Add option resolve_symlinks for shared collections
    To make public collections visible for everybody else
    one can use a hack and symlink all directories that a user
    has access to to the user collection. To actually get the right
    permissions on these files, the symlinks have to be followed to the
    actual radicale path. The option resolve_symlinks does just that.

--- radicale_rights_combine/__init__.py
+++ radicale_rights_combine/__init__.py
@@ -5,7 +5,9 @@ from radicale.rights import (
     OwnerOnlyRights,
     Rights,
 )
+from radicale.storage import load
 from importlib import import_module
+from os import path as p
 
 name = "radicale-rights-combine"
 builtin_rights = {
@@ -25,6 +27,15 @@ class Rights(BaseRights):
             s: self.init_right(s, configuration, logger)
             for s in types
         }
+        self.reslove_symlinks = self.configuration.getboolean(
+            "rights", "resolve_symlinks")
+        if self.resolve_symlinks:
+            filesystem_folder = self.configuration.get("storage",
+                                                       "filesystem_folder")
+            self.storepath = p.join(filesystem_folder, "collection_root")
+            self.authorized = self.__authorized_resolve_symlinks
+        else:
+            self.authorized = self.__authorized
 
     @staticmethod
     def init_right(right_name, configuration, logger):
@@ -33,15 +44,20 @@ class Rights(BaseRights):
         else:
             return import_module(right_name).Rights(configuration, logger)
 
-    def authorized(self, user, path, permissions):
+    def __authorized_resolve_symlinks(self, user, path, permissions):
+        fullpath = p.join(self.storepath, path)
+        if p.islink(fullpath):
+            target = p.realpath(fullpath)
+            path = target.replace(self.storepath, "")
+        return self.__authorized(user, path, permissions)
+
+    def __authorized(self, user, path, permissions):
         self.logger.debug(
             """Using plugins %r to determine rights for request:
                 User %r access path %r with permissions %r.""",
             self.rights.keys(), user, path, permissions)
-        results = (
-            right.authorized(user, path, permissions)
-            for right in self.rights.values()
-        )
+        results = (right.authorized(user, path, permissions)
+                   for right in self.rights.values())
         if any(results):
             self.logger.debug("One of the plugins authorized access.")
             return True
